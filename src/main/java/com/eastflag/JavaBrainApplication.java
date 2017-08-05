4678b1f0481e6f34169df906e79fea5741dfbaef
eastflag <eastflag@gmail.com>
2017-08-05 09:32:44 +0900

    spring integration, mqtt 라이브러리 추가

--- src/main/java/com/eastflag/JavaBrainApplication.java
+++ src/main/java/com/eastflag/JavaBrainApplication.java
@@ -1,9 +1,19 @@
 package com.eastflag;
 
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -43,4 +53,66 @@ public class JavaBrainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBrainApplication.class, args);
 	}
+
+	/*@Bean
+	public MessageChannel mqttInputChannel() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	public MessageProducer inbound() {
+		MqttPahoMessageDrivenChannelAdapter adapter =
+				new MqttPahoMessageDrivenChannelAdapter("tcp://localhost:1883", "testClient",
+						"topic1", "topic2");
+		adapter.setCompletionTimeout(5000);
+		adapter.setConverter(new DefaultPahoMessageConverter());
+		adapter.setQos(1);
+		adapter.setOutputChannel(mqttInputChannel());
+		return adapter;
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "mqttInputChannel")
+	public MessageHandler handler() {
+		return new MessageHandler() {
+
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				System.out.println(message.getPayload());
+			}
+
+		};
+	}
+	public static void startMqtt() {
+		final String MQTT_BROKER_IP = "tcp://www.javabrain.kr:1883";
+
+		try {
+			MqttClient client = new MqttClient(MQTT_BROKER_IP, MqttClient.generateClientId(), new MemoryPersistence());
+
+			client.connect();
+
+			client.setCallback(new MqttCallback() {
+				@Override
+				public void connectionLost(Throwable cause) {
+					System.out.println("connection lost");
+				}
+
+				@Override
+				public void messageArrived(String topic, MqttMessage message) throws Exception {
+					System.out.println("token: " + topic + ", message:" + message.toString());
+				}
+
+				@Override
+				public void deliveryComplete(IMqttDeliveryToken token) {
+					System.out.println("deliveryComplete");
+				}
+			});
+
+			client.subscribe("javabrain");
+
+		} catch(MqttException e) {
+			System.out.println(e.getMessage());
+		}
+	}*/
+
 }
--- src/test/java/com/eastflag/SpringTest.java
+++ src/test/java/com/eastflag/SpringTest.java
@@ -1,6 +1,5 @@
 package com.eastflag;
 
-import com.eastflag.config.DatasourceProfile;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringTest {
-    @Autowired
+/*    @Autowired
     private DatasourceProfile profile;
 
     @Test
     public void contextLoads() {
         System.out.println(profile.getUserName());
-    }
+    }*/
 
 }
