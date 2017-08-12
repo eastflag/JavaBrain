076363140b5d0d75525a679612ce720549ea764c
eastflag <eastflag@gmail.com>
2017-08-12 21:03:50 +0900

    랜던 테스트 추가

--- src/test/java/com/eastflag/MongoTest.java
+++ src/test/java/com/eastflag/MongoTest.java
@@ -7,9 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Random;
 
 @Ignore
 @RunWith(SpringRunner.class)
@@ -18,6 +22,10 @@ public class MongoTest {
     @Autowired
     CardsRepository repository;
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Ignore
     @Test
     public void findsByWords() {
         // 전체 숫자 카운트: repository에 정의되지 않은 함수
@@ -33,5 +41,21 @@ public class MongoTest {
         //List<CardsVO> cards = repository.findRandom(3);
 
         System.out.println(repository.findOne(174l));
+    }
+
+    @Test
+    public void templateTest() {
+        //CardsVO cards = mongoTemplate.executeCommand("db.cards.find().limit(1).skip(_rand()*db.cards.count())", CardsVO.class);
+        long count = repository.count();
+        Query query = new Query();
+
+        for(int i=0; i<10; ++i) {
+            int random = (int) (Math.random() * count) + 1;
+            query.skip(random).limit(1);
+            System.out.println("count:" + count + " random: " + random);
+
+            List<CardsVO> cards = mongoTemplate.find(query, CardsVO.class);
+            System.out.println(cards);
+        }
     }
 }
