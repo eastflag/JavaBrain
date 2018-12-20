e40f3e455a7c21ad83ac13840b1edbe9eff6cd35
eastflag <eastflag@gmail.com>
2018-12-20 14:44:08 +0900

    member 객체 json not null 만 리턴되게 수정

--- src/main/java/com/eastflag/domain/MemberVO.java
+++ src/main/java/com/eastflag/domain/MemberVO.java
@@ -1,7 +1,9 @@
 package com.eastflag.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class MemberVO {
     private Integer member_id;
